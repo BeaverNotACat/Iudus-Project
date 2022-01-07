f8c837ab0edb6b4aaf43ca9eb54cae5c0fe1db6b
BeaverNotACat <gub578208@gmail.com>
2022-01-08 00:02:51 +1000

    Исправления физики гг
    Убрал все остальные варианты перемещения, и смирился с еблей стен
    Плюс убрал лишние домножения на время между кадрами

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
 #Константные значения для физики
-export var ACCELERATION = 2500
+export var ACCELERATION = 25
 export var MAX_SPEED = 150
 export var FRICTION = 0.57
-export var DASH_SPEED = 10000
+export var DASH_SPEED = 100
 export var DASH_LENGHT = 0.2
 
 #Вспомогательные глобальные переменные
 var state = MOVE
 var velocity = Vector2.ZERO
-var DASH_DIRECTION #ОТ этой надо избавится
+var DASH_DIRECTION #ОТ этой надо избавится (Хотя я не придумал как сделать лучше, так что пусть будет~~)
 
 #Список состояний
 enum{
@@ -33,7 +33,7 @@ func _physics_process(delta):
 			move_character(delta)
 			get_action_input(delta)
 		DASH:
-			dash_state(delta, DASH_DIRECTION)
+			dash_state(DASH_DIRECTION)
 			get_action_input(delta)
 	print(velocity)
 ########################
@@ -55,17 +55,15 @@ func get_action_input(delta):
 #Двигнаем спрайт спрайт по физике плюс обрабатываем коллизии
 func move_character(delta):
 	if get_move_direction(delta) != Vector2.ZERO:
-		#velocity += get_move_direction(delta) * ACCELERATION * delta 
-		#velocity = velocity.clamped(MAX_SPEED)
+		velocity = velocity.clamped(MAX_SPEED)
 		velocity = velocity.move_toward(MAX_SPEED * get_move_direction(delta), ACCELERATION)
-		#velocity = velocity.linear_interpolate(get_move_direction(delta)* MAX_SPEED , FRICTION)
 	else:
 		velocity = velocity.linear_interpolate(Vector2.ZERO, FRICTION)
 	velocity = move_and_slide(velocity)
 
 #Двигаем спрайт со скоростью дэша
-func dash_state(delta, move_direction):
-	velocity += move_direction * DASH_SPEED * delta
+func dash_state(move_direction):
+	velocity += move_direction * DASH_SPEED 
 	velocity = move_and_slide(velocity)
 
 #Возвращаемся в стандартное состояние, по истечениюю таймера
